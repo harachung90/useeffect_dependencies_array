d1ed8daf28d917b3c44210a5d20113c3d44b50ee
Hara Chung <harachung90@gmail.com>
2023-01-07 18:19:20 +1100

    Second commit. Learned how to update dependencies

--- src/App.js
+++ src/App.js
@@ -3,25 +3,30 @@ import './App.css';
 
 function App() {
   const [starWarsData, setStarWarsData] = React.useState({})
-  const [count, setCount] = React.useState(0)
+  const [count, setCount] = React.useState(1)
 
   console.log("Component rendered")
 
-  /**
-   * Challenge: re-write the useEffect
-   * It should run any time `count` changes
-   * For now, just console.log("Effect function ran")
-   */
+    /**
+     * Challenge: Combine `count` with the request URL
+     * so pressing the "Get Next Character" button will
+     * get a new character from the Star Wars API.
+     * Remember: don't forget to consider the dependencies
+     * array!
+     */
 
   React.useEffect(()=>{
-    console.log("Effect function ran");
+    console.log(count);
+      fetch(`https://swapi.dev/api/people/${count}`)
+          .then(res => res.json())
+          .then(data => setStarWarsData(data))
   }, [count])
 
   return (
       <div>
-        <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
         <h2>The count is {count}</h2>
-        <button onClick={() => setCount(prevCount => prevCount + 1)}>Add</button>
+        <button onClick={() => setCount(prevCount => prevCount + 1)}>Get Next Character</button>
+        <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
       </div>
   )
 }
